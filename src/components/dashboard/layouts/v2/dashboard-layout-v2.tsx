55b3e85d01684b19cd6c272def25de4d6fe51c01
Alireza-irnp <alireza1iranpour@gmail.com>
2026-04-21 19:15:24 -0700

    Update dashboard-layout-v2.tsx

--- src/components/dashboard/layouts/v2/dashboard-layout-v2.tsx
+++ src/components/dashboard/layouts/v2/dashboard-layout-v2.tsx
@@ -37,10 +37,10 @@ export default function DashboardLayoutV2({
         <AppSidebarV2 isPinned={isPinned} onTogglePin={handleTogglePin} />
       </div>
 
-      <div className="flex flex-1 flex-col min-w-0 overflow-hidden">
+      <div className="flex flex-1 flex-col min-w-0 overflow-y-auto">
         <DashboardHeaderV2 />
 
-        <main className="flex flex-1 flex-col gap-4 overflow-auto p-4 pt-0">
+        <main className="flex flex-1 flex-col gap-4 p-4 pt-0">
           {children}
         </main>
       </div>
