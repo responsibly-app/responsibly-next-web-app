be10737172a1ab8c325e2aa389bc73f6c9e777b0
Alireza-irnp <alireza1iranpour@gmail.com>
2026-05-26 12:31:40 -0700

    simplified the loader with consistent strokeWidth

--- src/components/ui-custom/loader.tsx
+++ src/components/ui-custom/loader.tsx
@@ -60,12 +60,15 @@ const Loader = React.forwardRef<SVGSVGElement, LoaderProps>(
           cy="12"
           r="10"
           stroke="currentColor"
-          strokeWidth="4"
+          strokeWidth="3"
         />{" "}
         <path
           className="opacity-75"
-          fill="currentColor"
-          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="3"
+          strokeLinecap="round"
+          d="M12 2a10 10 0 0 1 10 10"
         />
       </svg>
     );
